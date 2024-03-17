a882f2332879e81e3a1a5292163fcd34f6e21cf5
Starwort <tcphone93@gmail.com>
2024-03-17 14:52:47 +0000

    Update localisation fixer to not cause crash-on-launch

--- Localization/TMLGetOffMyLocale.cs
+++ Localization/TMLGetOffMyLocale.cs
@@ -1,10 +1,9 @@
-﻿// Copied with permission from https://github.com/Cypheriel/UnnamedTechMod/blob/main/Common/Systems/LocalizationILEdit.cs
-using MonoMod.Cil;
-using MonoMod.RuntimeDetour;
+﻿// based on code from https://github.com/JavidPack/SummonersAssociation/blob/1.4/Items/MinionControlRod.cs
 using System.Reflection;
 using System;
 using Terraria.Localization;
 using Terraria.ModLoader;
+using MonoMod.RuntimeDetour;
 
 namespace BingoSyncGoalPack.Localization {
     /// <summary>
@@ -12,45 +11,33 @@ namespace BingoSyncGoalPack.Localization {
     /// This solves issues with tModLoader's autoformatting for hjson.
     /// </summary>
     internal class TMLGetOffMyLocale : ModSystem {
-        private static ILHook? hook;
+        internal delegate void orig_UpdateLocalizationFilesForMod(Mod mod, string? outputPath = null, GameCulture? specificCulture = null);
+        internal delegate void hook_UpdateLocalizationFilesForMod(orig_UpdateLocalizationFilesForMod orig, Mod mod, string? outputPath = null, GameCulture? specificCulture = null);
+
+        // private static void UpdateLocalizationFilesForMod(Mod mod, string outputPath = null, GameCulture specificCulture = null)
+        private static readonly MethodInfo m_UpdateLocalizationFiles = typeof(LocalizationLoader).GetMethod(
+            "UpdateLocalizationFilesForMod",
+            BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Default
+        ) ?? throw new InvalidOperationException("UpdateLocalizationFilesForMod method not found.");
+
+        private static Hook? hook;
 
         public override void Load() {
-            var methInfo = typeof(LocalizationLoader)
-                .GetMethod(
-                    "UpdateLocalizationFiles",
-                    BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Default
-                ) ?? throw new InvalidOperationException("UpdateLocalizationFiles method not found.");
+            try {
+                hook = new Hook(m_UpdateLocalizationFiles, new hook_UpdateLocalizationFilesForMod(noRuiningMyFiles));
+            } catch {
+                Mod.Logger.Error($"Failed to hook UpdateLocalizationFilesForMod!");
+            }
+        }
 
-            Mod.Logger.Debug("tModLoader wouldn't keep its grubby little hands off of my poor locale files.");
-            hook = new ILHook(methInfo, UpdateLocalizationFiles_ILEdit);
+        private void noRuiningMyFiles(orig_UpdateLocalizationFilesForMod orig, Mod mod, string? outputPath, GameCulture? specificCulture) {
+            if (mod is not BingoSyncGoalPack) {
+                orig(mod, outputPath, specificCulture);
+            }
         }
 
         public override void Unload() {
             hook?.Dispose();
-        }
-
-        private static void UpdateLocalizationFiles_ILEdit(
-            ILContext context
-        ) {
-            var methInfo = typeof(LocalizationLoader)
-                .GetMethod(
-                    "UpdateLocalizationFilesForMod",
-                    BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static
-                ) ?? throw new InvalidOperationException("UpdateLocalizationFilesForMod method not found.");
-            var methDelegate = methInfo
-                .CreateDelegate<Action<Mod, string, GameCulture>>();
-
-            var cursor = new ILCursor(context);
-            cursor.GotoNext(MoveType.Before,
-                instruction => instruction.MatchCall(methInfo)
-            );
-            cursor.Remove();
-            cursor.EmitDelegate<Action<Mod, string, GameCulture>>((mod, arg1, arg2) =>
-            {
-                if (mod is BingoSyncGoalPack)
-                    return;
-                methDelegate.Invoke(mod, arg1, arg2);
-            });
         }
     }
 }
